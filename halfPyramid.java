c887854399c9adf8ddd35975da7da5fb9ce6439c
prince <115145305+prince-1104@users.noreply.github.com>
2022-10-12 14:47:56 +0530

    Update halfPyramid.java

--- halfPyramid.java
+++ halfPyramid.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class halfPryamid{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter :");
         int n= sc.nextInt();
         
         // int n=5;
